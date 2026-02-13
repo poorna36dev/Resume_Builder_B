422172e288715243bb85b1aaaf97090e6c90a424
poorna36dev <poorna2429@gmail.com>
2026-02-13 10:06:50 +0530

    Update SecurityConfig.java

--- src/main/java/com/svu/resume/config/SecurityConfig.java
+++ src/main/java/com/svu/resume/config/SecurityConfig.java
@@ -44,6 +44,8 @@ public class SecurityConfig {
             .permitAll()
             .anyRequest().authenticated())
         .csrf(csrf->csrf.disable())
+        .formLogin(form -> form.disable())
+        .httpBasic(basic -> basic.disable())
         .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .addFilterBefore(jwtAuthFilter,UsernamePasswordAuthenticationFilter.class)
         .exceptionHandling(ex->ex.authenticationEntryPoint(new JwtAuthenticationEntryPoint()));
